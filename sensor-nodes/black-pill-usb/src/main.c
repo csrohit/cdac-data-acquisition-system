071441daaf75720e3e92c992bedac36b857fe6c3
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-02 16:58:04 +0530

    :zap: add callback functions

--- sensor-nodes/black-pill-usb/src/main.c
+++ sensor-nodes/black-pill-usb/src/main.c
@@ -4,21 +4,87 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/zephyr.h>
+#include <stdio.h>
+#include <string.h>
 #include <zephyr/device.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/usb/usb_ch9.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/zephyr.h>
+#include <zephyr/sys/ring_buffer.h>
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/logging/log.h>
 
 // register root level logger
 LOG_MODULE_REGISTER(usb_driver, CONFIG_LOG_DEFAULT_LEVEL);
 static const char c = ' ';
+
+#define RING_BUF_SIZE 1024
+uint8_t ring_buffer[RING_BUF_SIZE];
+
+struct ring_buf ringbuf;
+
+void ro_usb_dc_status_callback(enum usb_dc_status_code cb_status,
+							   const uint8_t *param);
+void ro_uart_irq_callback_user_data_t (const struct device *dev,
+									  void *user_data);
+
+
+									  static void interrupt_handler(const struct device *dev, void *user_data)
+{
+	ARG_UNUSED(user_data);
+
+	while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
+		if (uart_irq_rx_ready(dev)) {
+			int recv_len, rb_len;
+			uint8_t buffer[64];
+			size_t len = MIN(ring_buf_space_get(&ringbuf),
+					 sizeof(buffer));
+
+			recv_len = uart_fifo_read(dev, buffer, len);
+			if (recv_len < 0) {
+				LOG_ERR("Failed to read UART FIFO");
+				recv_len = 0;
+			};
+
+			rb_len = ring_buf_put(&ringbuf, buffer, recv_len);
+			if (rb_len < recv_len) {
+				LOG_ERR("Drop %u bytes", recv_len - rb_len);
+			}
+
+			LOG_DBG("tty fifo -> ringbuf %d bytes", rb_len);
+			if (rb_len) {
+				uart_irq_tx_enable(dev);
+			}
+		}
+
+		if (uart_irq_tx_ready(dev)) {
+			uint8_t buffer[64];
+			int rb_len, send_len;
+
+			rb_len = ring_buf_get(&ringbuf, buffer, sizeof(buffer));
+			if (!rb_len) {
+				LOG_DBG("Ring buffer empty, disable TX IRQ");
+				uart_irq_tx_disable(dev);
+				continue;
+			}
+
+			send_len = uart_fifo_fill(dev, buffer, rb_len);
+			if (send_len < rb_len) {
+				LOG_ERR("Drop %d bytes", rb_len - send_len);
+			}
+
+			LOG_DBG("ringbuf -> tty fifo %d bytes", send_len);
+		}
+	}
+}
+
+
 void main(void)
 {
 	const struct device *dev;
 	uint32_t dtr = 0U;
 	int ret;
+	uint32_t baudrate;
 
 	dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
 	if (!device_is_ready(dev))
@@ -28,7 +94,7 @@ void main(void)
 	}
 	LOG_INF("%cCDC ACM device is ready", c);
 
-	ret = usb_enable(NULL);
+	ret = usb_enable(ro_usb_dc_status_callback);
 	if (ret < 0)
 	{
 		LOG_ERR("%cusb_enable() failed", c);
@@ -36,14 +102,13 @@ void main(void)
 	}
 	LOG_INF("%cusb device enabled", c);
 
-	LOG_INF("%cWait for DTR", c);
+	LOG_INF("Wait for DTR%c", c);
 
 	while (true)
 	{
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		if (dtr)
 		{
-			LOG_INF("%cReceived DTR", c);
 			break;
 		}
 		else
@@ -52,4 +117,104 @@ void main(void)
 			k_sleep(K_MSEC(100));
 		}
 	}
+	LOG_INF("Received DTR%c", c);
+	/* They are optional, we use them to test the interrupt endpoint */
+	ret = uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
+	if (ret)
+	{
+		LOG_WRN("Failed to set DCD, ret code %d", ret);
+	}
+
+	ret = uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+	if (ret)
+	{
+		LOG_WRN("Failed to set DSR, ret code %d", ret);
+	}
+
+	/* Wait 1 sec for the host to do all settings */
+	k_busy_wait(1000000);
+
+	ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_BAUD_RATE, &baudrate);
+	if (ret)
+	{
+		LOG_WRN("Failed to get baudrate, ret code %d", ret);
+	}
+	else
+	{
+		LOG_INF("Baudrate detected: %d", baudrate);
+	}
+
+	// uart_irq_callback_user_data_set(dev, ro_uart_irq_callback_user_data_t, NULL);
+	// uart_irq_rx_enable(dev);
+		uart_irq_callback_set(dev, interrupt_handler);
+
+	/* Enable rx interrupts */
+	uart_irq_rx_enable(dev);
+}
+
+/**
+ * @brief
+ *
+ * @param cb_status
+ * @param param
+ */
+void ro_usb_dc_status_callback(enum usb_dc_status_code cb_status, const uint8_t *param)
+{
+	char *status = NULL;
+	switch (cb_status)
+	{
+	case USB_DC_ERROR:
+		status = "USB_DC_ERROR";
+		break;
+	case USB_DC_RESET:
+		status = "USB_DC_RESET";
+		break;
+	case USB_DC_CONNECTED:
+		status = "USB_DC_CONNECTED";
+		break;
+	case USB_DC_DISCONNECTED:
+		status = "USB_DC_DISCONNECTED";
+		break;
+	case USB_DC_CONFIGURED:
+		status = "USB_DC_CONFIGURED";
+		break;
+	case USB_DC_SUSPEND:
+		status = "USB_DC_SUSPEND";
+		break;
+	case USB_DC_RESUME:
+		status = "USB_DC_RESUME";
+		break;
+	case USB_DC_INTERFACE:
+		status = "USB_DC_INTERFACE";
+		break;
+	case USB_DC_CLEAR_HALT:
+		status = "USB_DC_CLEAR_HALT";
+		break;
+	case USB_DC_SOF:
+		status = "USB_DC_SOF";
+		break;
+	case USB_DC_UNKNOWN:
+		status = "USB_DC_UNKNOWN";
+		break;
+	case USB_DC_SET_HALT:
+		status = "USB_DC_ERROR";
+		break;
+	}
+	LOG_INF("%cro_usb_dc_status_callback(): param=%hu, status=%s", c, (unsigned short)(*param), status);
+}
+
+
+void ro_uart_irq_callback_user_data_t (const struct device *dev, void *user_data){
+	ARG_UNUSED(user_data);
+
+	while (uart_irq_update(dev) && uart_irq_is_pending(dev))
+	{
+		if(uart_irq_rx_ready(dev)){
+			LOG_INF("ro_uart_irq_callback_user_data_t(): RX is ready");
+		}
+		if(uart_irq_tx_ready(dev)){
+			LOG_INF("ro_uart_irq_callback_user_data_t(): TX is ready");
+		}
+	}
+	
 }
