ee79f34a4c59d2ce78db834d282d3c30f0d56161
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-02 12:01:29 +0530

    ⚰️ remove dead code

--- sensor-nodes/black-pill-usb/src/main.c
+++ sensor-nodes/black-pill-usb/src/main.c
@@ -11,54 +11,9 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/uart.h>
 
+// register root level logger
 LOG_MODULE_REGISTER(usb_driver, CONFIG_LOG_DEFAULT_LEVEL);
-
-// #define RO_EP_OUT_ADDR 0X01
-// #define RO_EP_IN_ADDR 0X81
-
-// void ro_out_cb(uint8_t ep,
-// 			   enum usb_dc_ep_cb_status_code cb_status);
-
-// void ro_in_cb(uint8_t ep,
-			//    enum usb_dc_ep_cb_status_code cb_status);
-// struct ro_usb_config
-// {
-// 	struct usb_if_descriptor if0;
-// 	struct usb_ep_descriptor if0_out_ep;
-// 	struct usb_ep_descriptor if0_in_ep;
-// } __packed;
-
-// USBD_CLASS_DESCR_DEFINE(primary, 0)
-// struct ro_usb_config ro_cfg = {
-// 	// interface descriptor
-// 	.if0 = {
-// 		.bLength = sizeof(struct usb_if_descriptor),
-// 		.bDescriptorType = USB_DESC_INTERFACE,
-// 		.bInterfaceNumber = 0,
-// 		.bAlternateSetting = 0,
-// 		.bNumEndpoints = 2,
-// 		.bInterfaceClass = USB_BCC_VENDOR,
-// 		.bInterfaceSubClass = 0,
-// 		.bInterfaceProtocol = 0,
-// 		.iInterface = 0},
-
-// 	// data endpoint out
-// 	.if0_out_ep = {.bLength = sizeof(struct usb_ep_descriptor), .bDescriptorType = USB_DESC_ENDPOINT, .bEndpointAddress =RO_EP_OUT_ADDR, .bInterval = 0x00, .wMaxPacketSize = 0x0040, .bmAttributes = USB_DC_EP_BULK},
-// 	.if0_in_ep = {.bLength = sizeof(struct usb_ep_descriptor), .bDescriptorType = USB_DESC_ENDPOINT, .bEndpointAddress = RO_EP_IN_ADDR, .bInterval = 0x00, .wMaxPacketSize = 0x0040, .bmAttributes = USB_DC_EP_BULK}
-
-// };
-
-// static struct usb_ep_cfg_data ep_cfg[] = {
-// 	{
-// 		.ep_cb = ro_out_cb,
-// 	 	.ep_addr = RO_EP_OUT_ADDR
-// 	 },
-// 	 {
-// 		.ep_cb = ro_in_cb,
-// 		.ep_addr = RO_EP_IN_ADDR
-// 	 }
-// };
-
+static const char c = ' ';
 void main(void)
 {
 	const struct device *dev;
@@ -66,42 +21,35 @@ void main(void)
 	int ret;
 
 	dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
-	if(!device_is_ready(dev)){
-		LOG_ERR("CDC ACM device not ready");
+	if (!device_is_ready(dev))
+	{
+		LOG_ERR("%cCDC ACM device not ready", c);
 		return;
 	}
-	LOG_INF("CDC ACM device is ready");
+	LOG_INF("%cCDC ACM device is ready", c);
 
 	ret = usb_enable(NULL);
-	if(ret < 0){
-		LOG_ERR("usb_enable() failed");
+	if (ret < 0)
+	{
+		LOG_ERR("%cusb_enable() failed", c);
 		return;
 	}
-	LOG_INF("usb device enabled");
+	LOG_INF("%cusb device enabled", c);
 
-	printk("Hello World! %s\n", "Rohit");
+	LOG_INF("%cWait for DTR", c);
 
-
-	LOG_INF("Wait for DTR");
-
-	while (true) {
+	while (true)
+	{
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-		if (dtr) {
-			LOG_INF("Received DTR");
+		if (dtr)
+		{
+			LOG_INF("%cReceived DTR", c);
 			break;
-		} else {
+		}
+		else
+		{
 			/* Give CPU resources to low priority threads. */
 			k_sleep(K_MSEC(100));
 		}
 	}
-
 }
-
-// void ro_out_cb(uint8_t ep,
-// 			   enum usb_dc_ep_cb_status_code cb_status)
-// {
-// }
-// void ro_in_cb(uint8_t ep,
-// 			   enum usb_dc_ep_cb_status_code cb_status)
-// {
-// }
