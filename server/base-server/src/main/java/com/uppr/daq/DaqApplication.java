d6d9f05458917953b06eda652c9c03373a169725
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-09 11:01:48 +0530

    🎉 start base project

---
+++ server/base-server/src/main/java/com/uppr/daq/DaqApplication.java
@@ -0,0 +1,13 @@
+package com.uppr.daq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaqApplication.class, args);
+	}
+
+}
