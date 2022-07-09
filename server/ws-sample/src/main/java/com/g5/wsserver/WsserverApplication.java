cf54e515c3932f31cee19861e4eb2b6fdb7cb829
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-09 07:31:48 +0530

    :sparkles: add demo project for wss

---
+++ server/ws-sample/src/main/java/com/g5/wsserver/WsserverApplication.java
@@ -0,0 +1,13 @@
+package com.g5.wsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsserverApplication.class, args);
+	}
+
+}
