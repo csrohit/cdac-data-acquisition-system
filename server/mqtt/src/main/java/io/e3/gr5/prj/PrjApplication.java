61f6b8b00906f50859d1d0f2cb2ac2cd577cd56a
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-09 07:21:41 +0530

    🚚 restructure directory

---
+++ server/mqtt/src/main/java/io/e3/gr5/prj/PrjApplication.java
@@ -0,0 +1,15 @@
+package io.e3.gr5.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjApplication.class, args);
+    }
+
+
+}
