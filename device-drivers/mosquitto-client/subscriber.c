f7845c3c97cd288357eb76364570bf228f2d3fe0
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-11 20:10:29 +0530

    🏗️ add makefile

---
+++ device-drivers/mosquitto-client/subscriber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<errno.h>   // to check errno variable
+#include<stdlib.h>
+#include<string.h>  
+#include<mosquitto.h>   // for mosquitto connections
+#include<mqtt_protocol.h>
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
