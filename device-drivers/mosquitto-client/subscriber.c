5f084456bee51a37212d290ad13c4364c06d97f8
Rohit Nimkar <nehalnimkar@gmail.com>
2022-07-12 08:14:48 +0530

    💥 async support

--- device-drivers/mosquitto-client/subscriber.c
+++ device-drivers/mosquitto-client/subscriber.c
@@ -6,9 +6,10 @@
 #include <mosquitto.h> // for mosquitto connections
 #include <mqtt_protocol.h>
 #include <signal.h>
-
+#include <stdint.h>
 #define DEBUG
 
+int mid = 12;
 const char *host = "localhost";
 const char *bind_address = "0.0.0.0";
 const int port = 1883U;
@@ -16,12 +17,21 @@ const char *topic = "usb/black-pill";
 const char *client_id = "usb_handler";
 const char *priv_data = "Data for all callbacks";
 struct mosquitto *client = NULL;
+uint8_t isSubscribed = 0;
 
+typedef struct {
+    uint8_t addr;
+    uint8_t cmd;
+    uint8_t len;
+} frame_t;
 
 // function declarations
 void termination_handler (int);
 void on_connect(struct mosquitto *, void *, int);
 void on_message(struct mosquitto *, void *, const struct mosquitto_message *);
+void on_disconnect(struct mosquitto *, void *, int);
+void on_subscribe(struct mosquitto *, void *, int, int, const int *);
+void on_unsubscribe(struct mosquitto *, void *, int);
 
 int main()
 {
@@ -52,7 +62,7 @@ int main()
     }
     printf("Mosquitto client initialised\n");
 
-    ret = mosquitto_connect_bind_v5(client, host, port, 10, bind_address, NULL);
+    ret = mosquitto_connect_async(client, host, port, 10);
     if (ret < 0)
     {
         switch (ret)
@@ -80,9 +90,12 @@ int main()
 
     mosquitto_connect_callback_set(client, on_connect);
     mosquitto_message_callback_set(client, on_message);
-
+    mosquitto_disconnect_callback_set(client, on_disconnect);
+    mosquitto_subscribe_callback_set(client, on_subscribe);
+    mosquitto_unsubscribe_callback_set(client, on_unsubscribe);
+    mosquitto_loop_start(client);
     mosquitto_loop_forever(client, 1000, 1);
-
+    printf("Stopped loop");
     CLEAN_CLIENT:
     // free memory associated with client
     mosquitto_destroy(client);
@@ -120,7 +133,12 @@ void on_connect(struct mosquitto *mosq, void * p_data, int return_code){
  */
 void on_message(struct mosquitto* mosq,void* obj,const struct mosquitto_message *msg){
     printf("Message from topic %s:%s\n",msg->topic,(char *)msg->payload);
-    printf("Message id: %d\n", msg->mid);
+    printf("Message id: %d, payload_len: %d\n", msg->mid, msg->payloadlen);
+    frame_t *t = (frame_t*) msg->payload;
+    printf("Addr: %#x, cmd: %#x, len: %#x\n", t->addr, t->cmd, t->len);
+    if(msg->payloadlen > 3){
+        printf("Data: %s\n", (char *)(msg->payload + 3));
+    }
 }
 
 /**
@@ -129,12 +147,37 @@ void on_message(struct mosquitto* mosq,void* obj,const struct mosquitto_message 
  * @param signal signal number
  */
 void termination_handler (int signal){
+
+    printf("Disconnecting client\n");
+    mosquitto_disconnect(client);
+
+    printf("Stopping loop\n");
+    mosquitto_loop_stop(client, 0);
+
     printf("Termination signal received\n");
     // free memory associated with client
-    printf("Destroyinh client\n");
+    printf("Destroying client\n");
     mosquitto_destroy(client);
     // free resources associated with library
     printf("Unloading library\n");
     mosquitto_lib_cleanup();
     _exit(EXIT_FAILURE);
+}
+
+void on_disconnect(struct mosquitto *m, void *priv_data, int ret_code){
+    printf("Disconnected from broker (%d)\n", ret_code);
+    mid = 12;
+    if(ret_code != 0){
+        mosquitto_reconnect_async(client);
+        mosquitto_unsubscribe(m, &mid, topic);
+    }
+}
+
+
+void on_subscribe(struct mosquitto *mosq, void *obj, int mid, int qos_count, const int *granted_qos){
+    printf("Subscribed to topic: %s, mid=%d\n", topic, mid);
+}
+
+void on_unsubscribe(struct mosquitto *mosq, void *obj, int mid){
+    printf("Unsubscribed from topic %s, mid=%d\n", topic, mid);
 }
