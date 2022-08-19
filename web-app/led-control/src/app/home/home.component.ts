2c329d26ad7512904adb886d700f6ecc24cf6767
Rohit Nimkar <nehalnimkar@gmail.com>
2022-08-19 10:19:48 +0530

    ✨ add home component

---
+++ web-app/led-control/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  /**
+   * Navigates to node list component
+   */
+  navigateToNode(): void{
+    this.router.navigate(['node']);
+  }
+
+}
