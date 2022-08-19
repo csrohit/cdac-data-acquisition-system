d4b0c885c08074ce0b30666d4ff82bddf33ff6fd
Rohit Nimkar <nehalnimkar@gmail.com>
2022-08-19 10:21:04 +0530

    ⚡️performance improvement

--- web-app/led-control/src/app/app-routing.module.ts
+++ web-app/led-control/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'node-type', loadChildren: () => import('./node-type/node-type.module').then(m => m.NodeTypeModule) },
   { path: 'node', loadChildren: () => import('./node/node.module').then(m => m.NodeModule) },
-  { path: 'device', loadChildren: () => import('./device/device.module').then(m => m.DeviceModule) }, 
-  { path: 'device-type', loadChildren: () => import('./device-type/device-type.module').then(m => m.DeviceTypeModule) }
+  { path: 'device', loadChildren: () => import('./device/device.module').then(m => m.DeviceModule) },
+  { path: 'device-type', loadChildren: () => import('./device-type/device-type.module').then(m => m.DeviceTypeModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
