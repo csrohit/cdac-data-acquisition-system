b613805d6e32dfdcb58b122266b23414c80eb5ff
Rohit Nimkar <nehalnimkar@gmail.com>
2022-08-19 10:17:12 +0530

    ✨ add node add component

---
+++ web-app/led-control/src/app/node/node-add/node-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-add',
+  templateUrl: './node-add.component.html',
+  styleUrls: ['./node-add.component.scss']
+})
+export class NodeAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
